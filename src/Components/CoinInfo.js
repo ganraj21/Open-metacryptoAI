6eb6dd15b5f62a92f6a13583054afcc35b827dfb
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2023-01-29 21:18:46 +0530

    working on coin page

---
+++ src/Components/CoinInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoinInfo = () => {
+  return <div>CoinInfo</div>;
+};
+
+export default CoinInfo;
--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,7 +1,78 @@
-import React from "react";
-
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CoinInfo from "../Components/CoinInfo";
+import { SingleCoin } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import ReactHtmlParser from "react-html-parser";
 const CoinPage = () => {
-  return <div>CoinPage</div>;
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+  console.log(coin);
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: "flex",
+      [theme.breakpoints.down("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+      },
+    },
+    sidebar: {
+      width: "30%",
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+      },
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: 25,
+      borderRight: "2px solid grey",
+    },
+    heading: {
+      fontWeight: "bold",
+      marginBottom: 20,
+      fontFamily: "Montserrat",
+    },
+  }));
+
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>
+        {/* sidebar */}
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height="200"
+          style={{ marginBottom: 20 }}
+        />
+      </div>
+      <Typography variant="h3" className={classes.heading}>
+        {coin?.name}
+      </Typography>
+      <Typography variant="subtitle1" classes={classes.description}>
+        {ReactHtmlParser(coin?.description.en.split(". ")[0])}
+      </Typography>
+
+      {/* chart */}
+      <CoinInfo coin={coin} />
+    </div>
+  );
 };
 
 export default CoinPage;
