32bba11b754f0f4f0db412c43763af79e68f316b
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2023-02-07 18:02:30 +0530

    change

--- metacryptoai/src/App.js
+++ metacryptoai/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Alerts from "./Components/Alerts";
+import Header from "./Components/Header";
+import CoinPage from "./Pages/CoinPage";
+import Homepage from "./Pages/Homepage";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    background: "#232629",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+      <Alerts />
+    </BrowserRouter>
   );
 }
 
