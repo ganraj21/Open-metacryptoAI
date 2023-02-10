f405184def41e4a17e7a893d3ea5c4f8d353f3a4
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2023-02-10 22:23:23 +0530

    change

--- src/Components/Authentication/AuthModal.js
+++ src/Components/Authentication/AuthModal.js
@@ -90,7 +90,7 @@ export default function AuthModal() {
               >
                 <span>OR</span>
                 <GoogleButton
-                  style={{ width: "90%", outline: "none", height: "45px" }}
+                  style={{ width: "90%", outline: "none" }}
                   onClick={signInWithGoogle}
                 />
               </div>
